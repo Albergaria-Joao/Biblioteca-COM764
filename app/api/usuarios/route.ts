75b95c9a86cfad6581b633564c43f7fe881042c5
João Albergaria <jvalbergariabar@gmail.com>
2026-03-11 00:30:52 -0300

    Prisma funcionando
    Downgrade do Prisma 6 p/ 7 porque não tem suporte p/ Mongo;
    
    Prisma Client gerado agora
    
    Chamada API corrigida

--- app/api/usuarios/route.ts
+++ app/api/usuarios/route.ts
@@ -1,6 +1,25 @@
-import { NextResponse } from "next/server"
-import prisma from "@/lib/prisma"
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
 
+// Cada função sempre vai ter o nome d o método 
 export async function GET() {
-    const usuarios = await prisma.Usuarios.findmany
+  try {
+
+    // Funciona que nem um select do SQL. Você escolhe as colunas e um limite (take)
+    const usuarios = await prisma.usuario.findMany({
+      take: 10,
+      select: {
+        id: true,
+        nome: true,
+        email: true,
+      },
+    });
+
+    return NextResponse.json(usuarios, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { status: 500 }
+    );
+  }
 }
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,19 @@
-import { PrismaClient } from "@prisma/client"
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export default prisma
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
