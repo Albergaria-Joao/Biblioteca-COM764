3dc4b3abb442c7b713443e808ac6cc9b3cbef087
João Albergaria <jvalbergariabar@gmail.com>
2026-03-19 22:09:51 -0300

    Criação de transação para cadastro
    Antes, se a criação de usuário falhasse, ficava um endereço solto.
    
    Agora, garante a atomicidade, como eu já havia dito em commit passado

--- app/api/usuarios/cadastro/route.ts
+++ app/api/usuarios/cadastro/route.ts
@@ -8,32 +8,46 @@ export async function POST(request: Request) {
   try {
     const body = await request.json();
     
-
-    const endereco = await prisma.endereco.create({
-        data: {
-            rua: body.rua,
-            cep: body.cep,
-            complemento: body.complemento,
-            numero: body.numero,
-            cidade: body.cidade,
-            estado: body.estado,
-            bairro: body.bairro
-        }
-    });
-
-    const usuario = await prisma.usuario.create({
-      data: {
-        nome: body.nome,
+    const usuarioExistente = await prisma.usuario.findUnique({
+      where: {
         email: body.email,
-        senha: await bcrypt.hash(body.senha, 10),
         cpf: body.cpf,
-        dataNascimento: body.dataNasc,
-        telefone: body.telefone,
-        enderecoId: endereco.id
       },
     });
 
-    return NextResponse.json(usuario, { status: 200 });
+    if (usuarioExistente) {
+      return NextResponse.json({ error: "Já existe um usuário com esse email ou CPF" }, { status: 400 });
+    }
+
+
+    const novoUsuario = await prisma.$transaction(async (prisma) => {
+      const endereco = await prisma.endereco.create({
+          data: {
+              rua: body.rua,
+              cep: body.cep,
+              complemento: body.complemento,
+              numero: body.numero,
+              cidade: body.cidade,
+              estado: body.estado,
+              bairro: body.bairro
+          }
+      });
+
+      const usuario = await prisma.usuario.create({
+        data: {
+          nome: body.nome,
+          email: body.email,
+          senha: await bcrypt.hash(body.senha, 10),
+          cpf: body.cpf,
+          dataNascimento: body.dataNasc,
+          telefone: body.telefone,
+          enderecoId: endereco.id
+        },
+      });
+      
+      return usuario;
+    });
+    return NextResponse.json(novoUsuario, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
